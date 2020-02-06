4de85e95350faa35facf6ce3ecfbb7dec0dd5497
shinkarenko2007 <matveishinkarenko363636@gmail.com>
2020-02-06 19:05:38 +1000

    4

--- 4_nested_rectangle.py
+++ 4_nested_rectangle.py
@@ -1,10 +1,23 @@
 import turtle
 
+
 def nested_rectangle():
-    for i in range(6):
-       turtle.right(10)
-       turtle.forward(10)
-    pass
+    turtle.shape('turtle')
+    x = 10
+    for i in range(10):
+        for j in range(4):
+            turtle.forward(x)
+            turtle.left(90)
+        turtle.penup()
+        turtle.left(180)
+        turtle.forward(20)
+        turtle.left(90)
+        turtle.forward(20)
+        turtle.left(90)
+        turtle.pendown()
+        x = x + 40
 
 if __name__ == "__main__":
     nested_rectangle()
+    while True:
+        pass
